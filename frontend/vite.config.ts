d458681413b221338fb150cd5812686d25e888ce
Sharath Kumar <sharathkumar967@example.com>
2026-06-22 14:14:08 +0530

    chore: allow all hosts for server and preview

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { port: Number(process.env.PORT) || 5173, host: '0.0.0.0', allowedHosts: ['prescripto-frontend-ndkw.onrender.com'] },
+  server: { port: Number(process.env.PORT) || 5173, host: '0.0.0.0', allowedHosts: true },
+  preview: { port: Number(process.env.PORT) || 5173, host: '0.0.0.0', allowedHosts: true },
 });
