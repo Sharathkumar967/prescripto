8c5a1f308a35f76fb2779a4bbd4a638a206d7d95
Sharath Kumar <sharathkumar967@example.com>
2026-06-22 14:24:28 +0530

    chore: allow all hosts for admin server and preview

--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { port: process.env.PORT || 5174, host: '0.0.0.0' },
+  server: { port: Number(process.env.PORT) || 5174, host: '0.0.0.0', allowedHosts: true },
+  preview: { port: Number(process.env.PORT) || 5174, host: '0.0.0.0', allowedHosts: true },
 });
